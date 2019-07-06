426e362cc897d0dc3111c02304c817273266bcb8
Petko <limona.pb@gmail.com>
2019-07-06 20:16:38 +0300

    Commit Box Volume Lateral Surface Area and surface area calculation application

---
+++ Box.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double height;
+        private double length;
+        private double width;
+
+        public Box(double length, double width, double height)
+        {
+            Height = height;
+            Length = length;
+            Width = width;
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                DataValidation(value, "Height");
+                this.height = value;
+            }
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                DataValidation(value, "Length");
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                DataValidation(value, "Width");
+                this.width = value;
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            return 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+        }
+
+        public double GetVolume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+
+        #region private
+        private void DataValidation(double value, string message)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException(message + " cannot be zero or negative.");
+            }
+        }
+
+        #endregion
+
+    }
+}
